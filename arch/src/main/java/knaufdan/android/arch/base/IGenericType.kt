f7a04d60f57de8861eb8d68523e062aa6218ee28
Daniel Knauf <hello.daniel@posteo.de>
2020-01-21 06:22:30 +0100

    Update IGenericType.kt

--- arch/src/main/java/knaufdan/android/arch/base/IGenericType.kt
+++ arch/src/main/java/knaufdan/android/arch/base/IGenericType.kt
@@ -5,7 +5,7 @@ import java.lang.reflect.ParameterizedType
 interface IGenericType <Type> {
 
     /**
-     * @return class type of [Type]
+     * @return class of [Type]
      */
     @Suppress("UNCHECKED_CAST")
     fun getTypeClass(): Class<Type> =
