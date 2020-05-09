fc6eaf7b7bca594116960120459d5e32aa772f6c
Daniel Knauf <daniel.knaut@posteo.de>
2020-05-09 14:59:59 +0200

    ComponentBinding
    - change var to val

--- arch/src/main/java/knaufdan/android/arch/databinding/recyclerview/ComponentAdapter.kt
+++ arch/src/main/java/knaufdan/android/arch/databinding/recyclerview/ComponentAdapter.kt
@@ -6,7 +6,7 @@ class ComponentAdapter<DataSource>(
     components: List<IComponent<DataSource>>
 ) : BaseAdapter<DataSource>() {
     // Store data in separate list to lose the reference and prevent error if references changes.
-    internal var dataSource = components.toList()
+    internal val dataSource = components.toList()
 
     override fun getLayoutRes(viewType: Int): Int = dataSource[viewType].getLayoutRes()
 
