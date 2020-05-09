73c09c3b1e6e9fd454abe7984db1967f411e1e84
Daniel Knauf <daniel.knaut@posteo.de>
2020-05-09 14:55:06 +0200

    ComponentBinding
    - do not set same items if adapter already holds items

--- arch/src/main/java/knaufdan/android/arch/databinding/recyclerview/ComponentAdapter.kt
+++ arch/src/main/java/knaufdan/android/arch/databinding/recyclerview/ComponentAdapter.kt
@@ -6,7 +6,7 @@ class ComponentAdapter<DataSource>(
     components: List<IComponent<DataSource>>
 ) : BaseAdapter<DataSource>() {
     // Store data in separate list to lose the reference and prevent error if references changes.
-    private val dataSource = components.toList()
+    internal var dataSource = components.toList()
 
     override fun getLayoutRes(viewType: Int): Int = dataSource[viewType].getLayoutRes()
 
