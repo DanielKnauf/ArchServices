70d4e35ef260747b37379adb86340272f8a68447
Daniel Knauf <daniel.knaut@posteo.de>
2020-05-16 08:32:24 +0200

    ISharedPrefService
    - make mode and location configurable
    - rename methods to reflect sharedPref methods

--- core/src/main/java/knaufdan/android/core/preferences/ISharedPrefService.kt
+++ core/src/main/java/knaufdan/android/core/preferences/ISharedPrefService.kt
@@ -1,37 +1,50 @@
 package knaufdan.android.core.preferences
 
+import android.content.Context
 import kotlin.reflect.KClass
 
 typealias Key = String
 
 interface ISharedPrefService {
-    fun saveAsJsonTo(
+    var sharedPrefLocation: String
+    var sharedPrefMode: Int
+
+    fun setup(
+        configure: ISharedPrefService.() -> Unit
+    )
+
+    fun putJson(
         key: Key,
         value: Any?
     )
 
-    fun saveTo(
+    fun put(
         key: Key,
         value: Any?
     )
 
-    fun <T : Any> retrieveJson(
+    fun <T : Any> getJson(
         key: Key,
         targetClass: KClass<T>
     ): T?
 
-    fun retrieveString(
+    fun getString(
         key: Key,
         defaultValue: String = ""
     ): String
 
-    fun retrieveLong(
+    fun getLong(
         key: Key,
         defaultValue: Long = 0
     ): Long
 
-    fun retrieveInt(
+    fun getInt(
         key: Key,
         defaultValue: Int = 0
     ): Int
+
+    companion object {
+        internal const val DEFAULT_LOCATION: String = "knaufdan.android.core.preferences"
+        internal const val DEFAULT_MODE: Int = Context.MODE_PRIVATE
+    }
 }
--- core/src/main/java/knaufdan/android/core/preferences/SharedPrefService.kt
+++ core/src/main/java/knaufdan/android/core/preferences/SharedPrefService.kt
@@ -1,6 +1,5 @@
 package knaufdan.android.core.preferences
 
-import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import android.util.Log
 import androidx.core.content.edit
@@ -9,27 +8,44 @@ import com.google.gson.JsonSyntaxException
 import javax.inject.Inject
 import javax.inject.Singleton
 import knaufdan.android.core.IContextProvider
+import knaufdan.android.core.preferences.ISharedPrefService.Companion.DEFAULT_LOCATION
+import knaufdan.android.core.preferences.ISharedPrefService.Companion.DEFAULT_MODE
 import kotlin.reflect.KClass
 
 @Singleton
 internal class SharedPrefService @Inject constructor(
     private val contextProvider: IContextProvider
 ) : ISharedPrefService {
-    private val sharedPrefLocation = "knaufdan.android.simpletimerapp.sharedPref"
+    override var sharedPrefLocation = DEFAULT_LOCATION
+    override var sharedPrefMode: Int = DEFAULT_MODE
     private val sharedPrefs
-        get() = contextProvider.getContext().getSharedPreferences(sharedPrefLocation, MODE_PRIVATE)
+        get() = contextProvider.getContext().getSharedPreferences(
+            sharedPrefLocation,
+            sharedPrefMode
+        )
+    private val gson: Gson by lazy {
+        Gson()
+    }
 
-    override fun saveAsJsonTo(
+    override fun setup(configure: ISharedPrefService.() -> Unit) {
+        configure(this)
+    }
+
+    override fun putJson(
         key: String,
         value: Any?
     ) {
-        saveTo(
+        if (value == null) {
+            return
+        }
+
+        put(
             key = key,
-            value = Gson().toJson(value)
+            value = gson.toJson(value)
         )
     }
 
-    override fun saveTo(
+    override fun put(
         key: String,
         value: Any?
     ) {
@@ -45,47 +61,49 @@ internal class SharedPrefService @Inject constructor(
         }
     }
 
-    private fun SharedPreferences.Editor.putValue(
-        key: String,
-        value: Any
-    ) {
-        when (value) {
-            is Int -> putInt(key, value)
-            is Long -> putLong(key, value)
-            is String -> putString(key, value)
-            is Enum<*> -> putString(key, value.name)
-            else -> Log.e(
-                "${SharedPrefService::class.simpleName}",
-                "Tried to store value of class ${value::class} to key $key but could not find corresponding method."
-            )
-        }
-    }
-
-    override fun <T : Any> retrieveJson(
+    override fun <T : Any> getJson(
         key: String,
         targetClass: KClass<T>
     ): T? =
         try {
-            retrieveString(key).run {
-                Gson().fromJson(this, targetClass.java)
+            getString(key).run {
+                gson.fromJson(this, targetClass.java)
             }
         } catch (e: JsonSyntaxException) {
             println(e)
             null
         }
 
-    override fun retrieveString(
+    override fun getString(
         key: String,
         defaultValue: String
     ): String = sharedPrefs.getString(key, defaultValue) ?: defaultValue
 
-    override fun retrieveLong(
+    override fun getLong(
         key: String,
         defaultValue: Long
     ): Long = sharedPrefs.getLong(key, defaultValue)
 
-    override fun retrieveInt(
+    override fun getInt(
         key: String,
         defaultValue: Int
     ): Int = sharedPrefs.getInt(key, defaultValue)
+
+    companion object {
+        private fun SharedPreferences.Editor.putValue(
+            key: String,
+            value: Any
+        ) {
+            when (value) {
+                is Int -> putInt(key, value)
+                is Long -> putLong(key, value)
+                is String -> putString(key, value)
+                is Enum<*> -> putString(key, value.name)
+                else -> Log.e(
+                    "${SharedPrefService::class.simpleName}",
+                    "Tried to store value of class ${value::class} to key $key but could not find corresponding method."
+                )
+            }
+        }
+    }
 }
