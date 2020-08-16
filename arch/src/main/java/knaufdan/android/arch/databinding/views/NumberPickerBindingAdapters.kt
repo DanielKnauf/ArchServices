0c327380605c813095b0da2e0d4c12f55e04d8fd
Daniel Knauf <daniel.knaut@posteo.de>
2020-08-16 21:53:17 +0200

    Number Picker Binding
    - improve suffix binding to prevent adding formatter multiple times
    - fix suffix for initial value

--- arch/src/main/java/knaufdan/android/arch/databinding/views/NumberPickerBindingAdapters.kt
+++ arch/src/main/java/knaufdan/android/arch/databinding/views/NumberPickerBindingAdapters.kt
@@ -1,9 +1,19 @@
 package knaufdan.android.arch.databinding.views
 
+import android.widget.EditText
 import android.widget.NumberPicker
 import androidx.databinding.BindingAdapter
+import java.util.WeakHashMap
 
-@BindingAdapter(value = ["min", "max", "isWrapped"])
+private val numberPickerFormatters: WeakHashMap<NumberPicker, NumberPicker.Formatter> = WeakHashMap()
+
+@BindingAdapter(
+    value = [
+        "min",
+        "max",
+        "isWrapped"
+    ]
+)
 fun NumberPicker.setup(
     min: Int,
     max: Int,
@@ -12,7 +22,23 @@ fun NumberPicker.setup(
     minValue = min
     maxValue = if (max < min) min else max
     wrapSelectorWheel = isWrapped
+
+    // NOTE: workaround for a bug that rendered the selected value wrong until user scrolled, see also: https://stackoverflow.com/q/27343772/3451975
+    (
+        NumberPicker::class.java.getDeclaredField("mInputText").apply {
+            isAccessible = true
+        }.get(this) as EditText
+        ).filters = emptyArray()
 }
 
 @BindingAdapter(value = ["suffix"])
-fun NumberPicker.configureFormatter(suffix: String) = setFormatter { value -> "$value $suffix" }
+fun NumberPicker.configureFormatter(suffix: String) {
+    val oldFormatter = numberPickerFormatters[this]
+    if (oldFormatter != null) {
+        return
+    }
+
+    val formatter = NumberPicker.Formatter { value -> "$value $suffix" }
+    numberPickerFormatters[this] = formatter
+    setFormatter(formatter)
+}
