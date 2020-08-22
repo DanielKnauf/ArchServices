657b9d0f271ef1604122a0bb837e1f53aee6132d
Daniel Knauf <daniel.knaut@posteo.de>
2020-08-22 14:14:22 +0200

    Provide way to define suffix for singular and plural

--- arch/src/main/java/knaufdan/android/arch/databinding/views/NumberPickerBindingAdapters.kt
+++ arch/src/main/java/knaufdan/android/arch/databinding/views/NumberPickerBindingAdapters.kt
@@ -2,10 +2,11 @@ package knaufdan.android.arch.databinding.views
 
 import android.widget.EditText
 import android.widget.NumberPicker
+import androidx.annotation.StringRes
 import androidx.databinding.BindingAdapter
 import java.util.WeakHashMap
 
-private val numberPickerFormatters: WeakHashMap<NumberPicker, NumberPicker.Formatter> = WeakHashMap()
+private val numberPickerFormatters = WeakHashMap<NumberPicker, NumberPicker.Formatter>()
 
 @BindingAdapter(
     value = [
@@ -25,20 +26,32 @@ fun NumberPicker.setup(
 
     // NOTE: workaround for a bug that rendered the selected value wrong until user scrolled, see also: https://stackoverflow.com/q/27343772/3451975
     (
-        NumberPicker::class.java.getDeclaredField("mInputText").apply {
-            isAccessible = true
-        }.get(this) as EditText
-        ).filters = emptyArray()
+        NumberPicker::class.java.getDeclaredField("mInputText")
+            .apply { isAccessible = true }
+            .get(this) as EditText
+        )
+        .filters = emptyArray()
 }
 
-@BindingAdapter(value = ["suffix"])
-fun NumberPicker.configureFormatter(suffix: String) {
+@BindingAdapter("suffixes")
+fun NumberPicker.configureFormatter(suffixes: Suffixes) {
     val oldFormatter = numberPickerFormatters[this]
     if (oldFormatter != null) {
         return
     }
 
-    val formatter = NumberPicker.Formatter { value -> "$value $suffix" }
+    val formatter = NumberPicker.Formatter { value ->
+        "$value ${resources.getString(suffixes.getSuffix(value))}"
+    }
+
     numberPickerFormatters[this] = formatter
+
     setFormatter(formatter)
+}
+
+data class Suffixes(
+    @StringRes private val singular: Int,
+    @StringRes private val plural: Int
+) {
+    fun getSuffix(count: Int) = if (count == 1) singular else plural
 }
