5f32f121a635144093df9c12bbc26e52012f9159
Daniel Knauf <daniel.knaut@posteo.de>
2020-11-12 21:23:51 +0100

    Notification Service
    - add documentation to interfaces
    - move  isAutoCancelEnabled from NotificationServiceConfig to NotificationConfig
    - set big text style on notification

--- services/src/main/java/knaufdan/android/services/userinteraction/notification/INotificationService.kt
+++ services/src/main/java/knaufdan/android/services/userinteraction/notification/INotificationService.kt
@@ -2,8 +2,30 @@ package knaufdan.android.services.userinteraction.notification
 
 import knaufdan.android.services.userinteraction.notification.api.NotificationConfig
 
+/**
+ * Provides functionality to configure a notification channel and send notification through it.
+ *
+ * NOTE: currently only one notification channel can be configure within [INotificationService],
+ *       and as it is a singleton only one channel can be provided within an application.
+ */
 interface INotificationService {
+    /**
+     * Adjusts config within notification channel using [INotificationServiceConfig] methods.
+     * Notification channel will be created based on config when first notification is sent.
+     *
+     * NOTE: valid config corresponding to the used Android version must be configured before calling [sendNotification]
+     *       otherwise no notification will be send.
+     *
+     * @param adjust extension function block on [INotificationServiceConfig] to configure [INotificationService] notification channel
+     */
     fun configure(adjust: INotificationServiceConfig.() -> Unit)
 
+    /**
+     * Sends a notification through the configured notification channel.
+     *
+     * NOTE: [configure] must be called before sending notifications otherwise all will be blocked.
+     *
+     * @param notificationConfig used to configure the notification
+     */
     fun sendNotification(notificationConfig: NotificationConfig)
 }
--- services/src/main/java/knaufdan/android/services/userinteraction/notification/INotificationServiceConfig.kt
+++ services/src/main/java/knaufdan/android/services/userinteraction/notification/INotificationServiceConfig.kt
@@ -1,6 +1,19 @@
 package knaufdan.android.services.userinteraction.notification
 
+/**
+ * Provides functionality to adjust the notification config within [INotificationService].
+ */
 interface INotificationServiceConfig {
+    /**
+     * Sets all parameters needed to create a notification channel.
+     *
+     * NOTE: Starting with Android O [26] a [channelName] and [channelDescription] are mandatory and must be not blank.
+     *
+     * @param channelId id of the channel
+     * @param channelName displayed name of the channel
+     * @param channelDescription displayed description of the channel
+     * @param channelImportance importance level of the channel
+     */
     fun setNotificationChannel(
         channelId: String,
         channelName: String = "",
@@ -8,7 +21,10 @@ interface INotificationServiceConfig {
         channelImportance: Int = 3 // == NotificationManager.IMPORTANCE_DEFAULT
     )
 
+    /**
+     * NOTE: per default vibrations are disabled.
+     *
+     * @param enabled if true vibration will be enabled when creating the notification channel.
+     */
     fun setVibration(enabled: Boolean)
-
-    fun setAutoCancel(enabled: Boolean = true)
 }
