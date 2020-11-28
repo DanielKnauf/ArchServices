16fa777e8f9360221a338e8e48d371007a35a247
Daniel Knauf <hello.daniel@posteo.de>
2020-11-28 10:31:20 +0100

    Update IAndroidBaseViewModel.kt

--- arch/src/main/java/knaufdan/android/arch/mvvm/IAndroidBaseViewModel.kt
+++ arch/src/main/java/knaufdan/android/arch/mvvm/IAndroidBaseViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.LifecycleObserver
 
 interface IAndroidBaseViewModel : LifecycleObserver {
     /**
-     * Notifies about the initialization of the corresponding [IBaseActivity] or [IBaseFragment].
+     * Notifies about the initialization of the associated [IBaseActivity] or [IBaseFragment].
      *
      * NOTE: this is called before the [IAndroidBaseViewModel] is bound to the layout.
      */
