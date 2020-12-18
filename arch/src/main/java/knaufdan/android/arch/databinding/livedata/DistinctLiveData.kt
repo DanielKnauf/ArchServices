eb244316091ad637c82f1fb06844242ea5daef80
Daniel Knauf <daniel.knaut@posteo.de>
2020-12-18 15:39:58 +0100

    Distinct live data
    - add docu

--- arch/src/main/java/knaufdan/android/arch/databinding/livedata/DistinctLiveData.kt
+++ arch/src/main/java/knaufdan/android/arch/databinding/livedata/DistinctLiveData.kt
@@ -2,6 +2,11 @@ package knaufdan.android.arch.databinding.livedata
 
 import androidx.lifecycle.MutableLiveData
 
+/**
+ *  A DistinctLiveData is a [MutableLiveData]. It compares new values
+ *  with its current value by using [equals]. If the DistinctLiveData already
+ *  holds the value, the new value is dropped and none of its observers is notified.
+ */
 class DistinctLiveData<T>(value: T) : MutableLiveData<T>(value) {
     override fun postValue(value: T) {
         if (this.value == value) {
