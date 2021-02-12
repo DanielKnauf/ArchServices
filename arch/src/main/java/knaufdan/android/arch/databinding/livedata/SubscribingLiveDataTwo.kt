2b5505fe73c5331f00a3064bffaec987acf59922
Daniel Knauf <daniel.knaut@posteo.de>
2021-02-12 20:50:18 +0100

    SubscribingLiveData
    - update documentation
    - rename SubscribingLiveDateTwoSources to ...Two

---
+++ arch/src/main/java/knaufdan/android/arch/databinding/livedata/SubscribingLiveDataTwo.kt
@@ -0,0 +1,44 @@
+package knaufdan.android.arch.databinding.livedata
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+
+/**
+ * A SubscribingLiveDataTwo is a [MediatorLiveData] which [subscribeTo] changes of [firstSource]
+ * and [secondSource] when first active observer is attached as well as removes [firstSource] and
+ * [secondSource] when last observer detaches. New values of [SubscribingLiveData] are determined
+ * by [mapping] function [FirstSourceType] as well as [SecondSourceType] to [TargetType] and
+ * posted asynchronously.
+ *
+ * @param firstSource [LiveData] added as first source
+ * @param FirstSourceType type of data hold by [firstSource]
+ * @param secondSource [LiveData] added as second source
+ * @param SecondSourceType type of data hold by [secondSource]
+ * @param TargetType type of data hold by target
+ * @param distinctUntilChanged if true [mapping] results equal to current value are discarded
+ * @param mapping function used to determine new value based on [firstSource] and [secondSource] values
+ */
+class SubscribingLiveDataTwo<FirstSourceType, SecondSourceType, TargetType>(
+    private val firstSource: LiveData<FirstSourceType>,
+    private val secondSource: LiveData<SecondSourceType>,
+    private val distinctUntilChanged: Boolean = true,
+    private val mapping: (FirstSourceType?, SecondSourceType?) -> TargetType
+) : MediatorLiveData<TargetType>() {
+    override fun onActive() {
+        super.onActive()
+
+        subscribeTo(
+            firstSource = firstSource,
+            secondSource = secondSource,
+            distinctUntilChanged = distinctUntilChanged,
+            mapping = mapping
+        )
+    }
+
+    override fun onInactive() {
+        removeSource(firstSource)
+        removeSource(secondSource)
+
+        super.onInactive()
+    }
+}
