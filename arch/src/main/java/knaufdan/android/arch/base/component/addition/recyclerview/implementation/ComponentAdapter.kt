b4fc1e6a178d8691073b07edf5e01186bfdbcc36
Daniel Knauf <daniel.knaut@posteo.de>
2021-06-29 07:10:47 +0200

    IComponent
    - add default binding key
    - clean up component adapter

--- arch/src/main/java/knaufdan/android/arch/base/component/addition/recyclerview/implementation/ComponentAdapter.kt
+++ arch/src/main/java/knaufdan/android/arch/base/component/addition/recyclerview/implementation/ComponentAdapter.kt
@@ -3,16 +3,14 @@ package knaufdan.android.arch.base.component.addition.recyclerview.implementatio
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.ListAdapter
 import knaufdan.android.arch.base.LayoutRes
 import knaufdan.android.arch.base.component.IComponent
 import knaufdan.android.arch.utils.findLifecycleOwner
 
 class ComponentAdapter(
-    components: List<IComponent<Any>>
+    components: List<IComponent<Any>> = emptyList()
 ) : ListAdapter<IComponent<Any>, ComponentViewHolder>(ComponentDiffCallback()) {
-    private lateinit var dataSource: List<IComponent<Any>>
 
     init {
         submitList(components)
@@ -22,36 +20,25 @@ class ComponentAdapter(
         parent: ViewGroup,
         viewType: LayoutRes
     ): ComponentViewHolder =
-        DataBindingUtil.inflate<ViewDataBinding>(
-            LayoutInflater.from(parent.context),
-            viewType,
-            parent,
-            false
-        ).run {
-            ComponentViewHolder(
-                binding = this,
-                lifeCycleOwner = parent.context.findLifecycleOwner()
-            )
-        }
+        ComponentViewHolder(
+            binding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                viewType,
+                parent,
+                false
+            ),
+            lifeCycleOwner = parent.context.findLifecycleOwner()
+        )
 
     override fun onBindViewHolder(
         holder: ComponentViewHolder,
         position: Int
     ) {
-        holder.bind(
-            component = dataSource[position]
-        )
+        holder.bind(getItem(position))
     }
 
     override fun getItemViewType(position: Int): LayoutRes =
-        dataSource[position].getLayoutRes()
-
-    override fun submitList(list: List<IComponent<Any>>?) {
-        // Store data in separate list to lose the reference and prevent error if referenced data changes.
-        dataSource = list?.toList() ?: return
-
-        super.submitList(dataSource)
-    }
+        getItem(position).getLayoutRes()
 
     override fun onViewAttachedToWindow(holder: ComponentViewHolder) {
         super.onViewAttachedToWindow(holder)
