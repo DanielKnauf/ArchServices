2fe5de28eed6bda9b13c02df1e3a4dea1ff343fc
Daniel Knauf <daniel.knaut@posteo.de>
2021-10-07 07:09:25 +0200

    Time picker
    - change to new material picker
    - remove deprecated function
    - add documentation

--- core/src/main/java/knaufdan/android/core/calendar/timepicker/TimePickerConfig.kt
+++ core/src/main/java/knaufdan/android/core/calendar/timepicker/TimePickerConfig.kt
@@ -3,10 +3,18 @@ package knaufdan.android.core.calendar.timepicker
 import androidx.annotation.StyleRes
 import knaufdan.android.core.calendar.Hour
 import knaufdan.android.core.calendar.Minute
+import knaufdan.android.core.calendar.getTimeOfDay
 import knaufdan.android.core.resources.IResourceProvider
 
+/**
+ * Used in [ITimePickerService.showTimePicker] to configure the displayed time picker.
+ *
+ * @param initialTime hours and minutes initially selected
+ * @param is24HourView defines if clock is displayed as 12 hours (am/pm) or 24 hours
+ * @param theme style to customize the displayed date picker
+ */
 data class TimePickerConfig(
-    val initialTime: Pair<Hour, Minute>,
+    val initialTime: Pair<Hour, Minute> = getTimeOfDay(),
     val is24HourView: Boolean = true,
     @StyleRes val theme: Int = IResourceProvider.INVALID_RES_ID
 )
---
+++ core/src/main/java/knaufdan/android/core/calendar/timepicker/implementation/TimePickerService.kt
@@ -0,0 +1,42 @@
+package knaufdan.android.core.calendar.timepicker.implementation
+
+import androidx.fragment.app.DialogFragment.STYLE_NO_TITLE
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.MaterialTimePicker.INPUT_MODE_CLOCK
+import com.google.android.material.timepicker.TimeFormat.CLOCK_12H
+import com.google.android.material.timepicker.TimeFormat.CLOCK_24H
+import knaufdan.android.core.IContextProvider
+import knaufdan.android.core.calendar.Hour
+import knaufdan.android.core.calendar.Minute
+import knaufdan.android.core.calendar.timepicker.ITimePickerService
+import knaufdan.android.core.calendar.timepicker.TimePickerConfig
+
+internal class TimePickerService(
+    private val contextProvider: IContextProvider,
+) : ITimePickerService {
+
+    override fun showTimePicker(
+        config: TimePickerConfig,
+        onCancelClicked: () -> Unit,
+        onTimeSelected: (Pair<Hour, Minute>) -> Unit
+    ) {
+        contextProvider.fragmentManager?.run {
+            MaterialTimePicker
+                .Builder()
+                .setInputMode(INPUT_MODE_CLOCK)
+                .setHour(config.initialTime.first)
+                .setMinute(config.initialTime.second)
+                .setTimeFormat(if (config.is24HourView) CLOCK_24H else CLOCK_12H)
+                .build()
+                .apply {
+                    setStyle(STYLE_NO_TITLE, config.theme)
+
+                    addOnPositiveButtonClickListener { onTimeSelected(hour to minute) }
+
+                    addOnCancelListener { onCancelClicked() }
+                    addOnNegativeButtonClickListener() { onCancelClicked() }
+                }
+                .show(this, this@TimePickerService.javaClass.simpleName)
+        }
+    }
+}
