c2da6214e4fc9f45207d550f351566c522385e03
Daniel Knauf <daniel.knaut@posteo.de>
2021-10-08 07:15:04 +0200

    Broadcast Service
    - add documentation

--- services/src/main/java/knaufdan/android/services/service/broadcast/IBroadcastService.kt
+++ services/src/main/java/knaufdan/android/services/service/broadcast/IBroadcastService.kt
@@ -2,23 +2,47 @@ package knaufdan.android.services.service.broadcast
 
 import android.content.BroadcastReceiver
 import android.content.Intent
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import kotlin.reflect.KClass
 
+/**
+ * [IBroadcastService] is an injectable service providing functionality for (un-)registering
+ * [BroadcastReceiver] at the [LocalBroadcastManager] and send out Intents to [BroadcastReceiver].
+ */
 interface IBroadcastService {
 
     /**
-     * Registers the [ActionBroadcastReceiver] at the [LocalBroadcastManager]
-     * and adds the [ActionBroadcastReceiver.getSupportedActions] to the [IntentFilter].
+     * Registers a [ActionBroadcastReceiver] at the LocalBroadcastManager.
+     * [ActionBroadcastReceiver.forActions] defines for which actions the [receiver] is registered.
+     *
+     * @param receiver [ActionBroadcastReceiver] to handle the broadcast
      */
-    fun registerLocalBroadcastReceiver(actionBroadcastReceiver: ActionBroadcastReceiver)
+    fun registerLocalBroadcastReceiver(receiver: ActionBroadcastReceiver)
 
-    fun unregisterLocalBroadcastReceiver(broadcastReceiver: BroadcastReceiver)
+    /**
+     * Unregister [BroadcastReceiver] from the LocalBroadcastManager
+     *
+     * @param receiver [BroadcastReceiver] to unregister.
+     */
+    fun unregisterLocalBroadcastReceiver(receiver: BroadcastReceiver)
 
+    /**
+     * Sends an intent to all interested BroadcastReceivers.
+     *
+     * @param receiver [KClass] to handle the intent.
+     * @param action [IntentAction] to perform by the [receiver]
+     */
     fun <T : BroadcastReceiver> sendBroadcast(
         receiver: KClass<T>,
         action: IntentAction
     )
 
+    /**
+     * Sends an intent to all interested BroadcastReceivers.
+     *
+     * @param action [IntentAction] to be performed
+     * @param configure extension function block to manipulate the created Intent
+     */
     fun sendBroadcast(
         action: IntentAction,
         configure: Intent.() -> Unit
