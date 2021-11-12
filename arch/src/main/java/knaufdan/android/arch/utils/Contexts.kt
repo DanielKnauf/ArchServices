a4fcc6406ca3387ef919646ce824d6e54552ff2f
Daniel Knauf <daniel.knaut@posteo.de>
2021-11-12 07:36:17 +0100

    RecyclerviewBindings
    - rename ContextExtensions to Contexts
    - add method to set itemDecoration via binding

--- arch/src/main/java/knaufdan/android/arch/utils/Contexts.kt
+++ arch/src/main/java/knaufdan/android/arch/utils/Contexts.kt
@@ -2,8 +2,12 @@ package knaufdan.android.arch.utils
 
 import android.content.Context
 import android.content.ContextWrapper
+import android.util.TypedValue
 import androidx.lifecycle.LifecycleOwner
 
 internal tailrec fun Context?.findLifecycleOwner(): LifecycleOwner? =
     if (this is LifecycleOwner) this
     else (this as? ContextWrapper)?.baseContext?.findLifecycleOwner()
+
+fun Context.dpToPx(dp: Number): Float =
+    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), resources.displayMetrics)
