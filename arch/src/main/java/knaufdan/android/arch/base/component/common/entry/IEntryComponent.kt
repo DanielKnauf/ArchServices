40a53e6e483203801168c0df6b0df9b6d969f6c4
Daniel Knauf <daniel.knaut@posteo.de>
2021-11-24 20:25:53 +0100

    EntryComponent
    - build entry component
    - create common text color

---
+++ arch/src/main/java/knaufdan/android/arch/base/component/common/entry/IEntryComponent.kt
@@ -0,0 +1,6 @@
+package knaufdan.android.arch.base.component.common.entry
+
+import knaufdan.android.arch.base.component.IComponent
+import knaufdan.android.arch.base.component.common.entry.implementation.EntryViewModel
+
+interface IEntryComponent : IComponent<EntryViewModel>
