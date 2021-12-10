0b6ef84350bcf43fd5c68e2a435167bf1e97cb68
Daniel Knauf <daniel.knaut@posteo.de>
2021-12-10 07:14:25 +0100

    View extensions
    - rotate view animated
    - change color of view property animated
    - dpToPx conversion

--- arch/src/main/java/knaufdan/android/arch/utils/Views.kt
+++ arch/src/main/java/knaufdan/android/arch/utils/Views.kt
@@ -1,6 +1,11 @@
 package knaufdan.android.arch.utils
 
+import android.animation.AnimatorListenerAdapter
+import android.animation.ArgbEvaluator
+import android.animation.ValueAnimator
 import android.view.View
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
 
 fun View.doOnAttachedToWindow(block: (View?) -> Unit) {
 
@@ -20,3 +25,36 @@ fun View.doOnAttachedToWindow(block: (View?) -> Unit) {
         }
     )
 }
+
+fun View.rotateAnimated(
+    rotation: Float,
+    durationInMillis: Long = 200
+) {
+    animate()
+        .setDuration(durationInMillis)
+        .setListener(object : AnimatorListenerAdapter() {})
+        .rotation(rotation)
+}
+
+fun View.changeColorAnimated(
+    @ColorRes from: Int,
+    @ColorRes to: Int,
+    durationInMillis: Long = 200,
+    onColorUpdate: (Int) -> Unit
+) {
+    ValueAnimator.ofObject(
+        ArgbEvaluator(),
+        ContextCompat.getColor(context, from),
+        ContextCompat.getColor(context, to)
+    ).apply {
+        duration = durationInMillis
+
+        addUpdateListener { animator ->
+            onColorUpdate(animator.animatedValue as Int)
+        }
+
+        start()
+    }
+}
+
+fun View.dpToPx(dp: Number): Float = context.dpToPx(dp)
