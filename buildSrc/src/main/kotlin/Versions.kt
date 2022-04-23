21338185957248ae4c6eacc8d9be855e3e213416
Daniel Knauf <daniel.knaut@posteo.de>
2022-04-23 14:10:39 +0900

    Dependency Updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,55 +1,55 @@
 object Versions {
 
     object Android {
-        const val vGradle = "4.2.1"
+        const val vGradle = "7.1.3"
     }
 
     object AndroidX {
-        const val vAppCompat = "1.3.1"
-        const val vBroadcastManager = "1.0.0"
-        const val vConstraintLayout = "2.1.0"
-        const val vCore = "1.6.0"
-        const val vFragment = "1.3.6"
+        const val vAppCompat = "1.4.1"
+        const val vBroadcastManager = "1.1.0"
+        const val vBrowser = "1.4.0"
+        const val vConstraintLayout = "2.1.3"
+        const val vCore = "1.7.0"
+        const val vFragment = "1.4.1"
         const val vLegacySupport = "1.0.0"
-        const val vLifecycle = "2.3.1"
+        const val vLifecycle = "2.4.1"
         const val vLifecycleExtensions = "2.2.0"
-        const val vNavigation = "2.3.5"
+        const val vNavigation = "2.5.0-alpha04"
         const val vRecyclerView = "1.2.1"
-        const val vRoom = "2.3.0"
-        const val vPaging = "3.0.1"
+        const val vRoom = "2.4.2"
+        const val vPaging = "3.1.1"
         const val vTestRunner = "1.4.0"
         const val vTransition = "1.4.1"
         const val vViewPager2 = "1.0.0"
     }
 
     object DK {
-        const val vArchServices = "master-SNAPSHOT"
-        const val vLiveDataKit = "0.1.1"
+        const val vLiveDataKit = "0.1.2"
     }
 
     object Google {
-        const val vDagger = "2.35.1"
-        const val vFirebaseCrashlytics = "18.2.1"
-        const val vFirebaseAnalytics = "19.0.1"
-        const val vFirebaseCrashlyticsPlugin = "2.7.1"
-        const val vGson = "2.8.8"
-        const val vHilt = "2.35.1"
-        const val vMaterial = "1.4.0"
+        const val vDagger = "2.41"
+        const val vFirebaseCrashlytics = "18.2.9"
+        const val vFirebaseAnalytics = "20.1.2"
+        const val vFirebaseCrashlyticsPlugin = "2.8.1"
+        const val vGson = "2.9.0"
+        const val vHilt = "2.41"
+        const val vMaterial = "1.5.0"
         const val vServices = "4.3.10"
     }
 
     object Kotlin {
-        const val vKotlin = "1.5.31"
-        const val vCoroutines = "1.5.2"
+        const val vKotlin = "1.6.20"
+        const val vCoroutines = "1.6.1"
     }
 
     object Other {
         const val vAndroidMaven = "2.1"
-        const val vDependencyUpdates = "0.39.0"
-        const val vKtLint = "0.42.1"
+        const val vDependencyUpdates = "0.42.0"
+        const val vKtLint = "0.43.2"
         const val vJUnit = "4.13.2"
         const val vPicasso = "2.71828"
         const val vRetrofit = "2.9.0"
-        const val vUCrop = "2.2.7"
+        const val vUCrop = "2.2.8"
     }
 }
