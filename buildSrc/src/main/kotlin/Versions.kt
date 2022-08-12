199830cc677eea194360dcbcc52cfc9e5e5a7de7
Daniel Knauf <daniel.knaut@posteo.de>
2022-08-12 06:50:04 +0200

    Dependency Updates

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,22 +1,22 @@
 object Versions {
 
     object Android {
-        const val vGradle = "7.1.3"
+        const val vGradle = "7.2.2"
     }
 
     object AndroidX {
-        const val vAppCompat = "1.4.1"
+        const val vAppCompat = "1.5.0"
         const val vBroadcastManager = "1.1.0"
         const val vBrowser = "1.4.0"
-        const val vConstraintLayout = "2.1.3"
-        const val vCore = "1.7.0"
-        const val vFragment = "1.4.1"
+        const val vConstraintLayout = "2.1.4"
+        const val vCore = "1.8.0"
+        const val vFragment = "1.5.2"
         const val vLegacySupport = "1.0.0"
-        const val vLifecycle = "2.4.1"
+        const val vLifecycle = "2.5.1"
         const val vLifecycleExtensions = "2.2.0"
-        const val vNavigation = "2.5.0-alpha04"
+        const val vNavigation = "2.5.1"
         const val vRecyclerView = "1.2.1"
-        const val vRoom = "2.4.2"
+        const val vRoom = "2.4.3"
         const val vPaging = "3.1.1"
         const val vTestRunner = "1.4.0"
         const val vTransition = "1.4.1"
@@ -24,22 +24,22 @@ object Versions {
     }
 
     object DK {
-        const val vLiveDataKit = "0.1.2"
+        const val vLiveDataKit = "0.2.0"
     }
 
     object Google {
-        const val vDagger = "2.41"
+        const val vDagger = "2.43.2"
         const val vFirebaseCrashlytics = "18.2.9"
         const val vFirebaseAnalytics = "20.1.2"
         const val vFirebaseCrashlyticsPlugin = "2.8.1"
-        const val vGson = "2.9.0"
+        const val vGson = "2.9.1"
         const val vHilt = "2.41"
-        const val vMaterial = "1.5.0"
+        const val vMaterial = "1.6.1"
         const val vServices = "4.3.10"
     }
 
     object Kotlin {
-        const val vKotlin = "1.6.21"
+        const val vKotlin = "1.7.10"
         const val vCoroutines = "1.6.1"
     }
 
@@ -47,7 +47,7 @@ object Versions {
         const val vAndroidMaven = "2.1"
         const val vCoil = "2.1.0"
         const val vDependencyUpdates = "0.42.0"
-        const val vKtLint = "0.45.2"
+        const val vKtLint = "0.46.1"
         const val vJUnit = "4.13.2"
         const val vPicasso = "2.71828"
         const val vRetrofit = "2.9.0"
