ca8d4373ffdcecc4618bdf430b73d93f16f63c10
Daniel Knauf <daniel.knaut@posteo.de>
2022-09-26 06:39:30 +0200

    IFragmentAnimation
    - create animation xmls
    - create sealed interface defining a fragment animation

---
+++ arch/src/main/java/knaufdan/android/arch/navigation/IFragmentAnimation.kt
@@ -0,0 +1,79 @@
+package knaufdan.android.arch.navigation
+
+import androidx.annotation.AnimRes
+import knaufdan.android.arch.R
+
+sealed interface IFragmentAnimation {
+
+    @get:AnimRes
+    val enter: Int
+
+    @get:AnimRes
+    val exit: Int
+
+    @get:AnimRes
+    val popEnter: Int
+
+    @get:AnimRes
+    val popExit: Int
+
+    object FadeInFadeOut : IFragmentAnimation {
+
+        override val enter: Int
+            get() = R.anim.arch_fade_in
+
+        override val exit: Int
+            get() = R.anim.arch_fade_out
+
+        override val popEnter: Int
+            get() = R.anim.arch_fade_in
+
+        override val popExit: Int
+            get() = R.anim.arch_fade_out
+    }
+
+    object SlideInFadeOut : IFragmentAnimation {
+
+        override val enter: Int
+            get() = R.anim.arch_slide_in_right
+
+        override val exit: Int
+            get() = R.anim.arch_fade_out
+
+        override val popEnter: Int
+            get() = R.anim.arch_fade_in
+
+        override val popExit: Int
+            get() = R.anim.arch_slide_out_right
+    }
+
+    object SlideInSlideOut : IFragmentAnimation {
+
+        override val enter: Int
+            get() = R.anim.arch_slide_in_right
+
+        override val exit: Int
+            get() = R.anim.arch_slide_out_left
+
+        override val popEnter: Int
+            get() = R.anim.arch_slide_in_left
+
+        override val popExit: Int
+            get() = R.anim.arch_slide_out_right
+    }
+
+    object SlideUpFadeOut : IFragmentAnimation {
+
+        override val enter: Int
+            get() = R.anim.arch_slide_up
+
+        override val exit: Int
+            get() = R.anim.arch_fade_out
+
+        override val popEnter: Int
+            get() = R.anim.arch_fade_in
+
+        override val popExit: Int
+            get() = R.anim.arch_slide_down
+    }
+}
