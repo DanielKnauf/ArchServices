7689b0625bfaa49a83e08ff4a3a545ba80c0cff8
Daniel Knauf <daniel.knaut@posteo.de>
2022-10-23 19:36:43 +0200

    Dependency Updates

--- arch/src/main/java/knaufdan/android/arch/utils/Contexts.kt
+++ arch/src/main/java/knaufdan/android/arch/utils/Contexts.kt
@@ -2,7 +2,6 @@ package knaufdan.android.arch.utils
 
 import android.content.Context
 import android.content.ContextWrapper
-import android.content.pm.PackageManager
 import android.util.TypedValue
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
@@ -18,6 +17,3 @@ fun Context.dpToPx(dp: Number): Float =
     TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), resources.displayMetrics)
 
 fun Context.getColorCompat(@ColorRes color: Int): Int = ContextCompat.getColor(this, color)
-
-fun Context.hasPermission(permission: String): Boolean =
-    ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
